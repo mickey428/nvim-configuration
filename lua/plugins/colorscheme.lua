d8aba6e4cc54cfcec15431e009d63f10a9a32c46
Miguel Tapia <tubiback@hotmail.com>
2025-06-24 01:32:09 -0400

    initial add of files for nvim configuration

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,9 @@
+return {
+    -- Shortened Github Url
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        vim.cmd.colorscheme "tokyonight-night"
+    end
+}
